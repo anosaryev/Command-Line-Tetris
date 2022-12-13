69d1c2214d98930eab484530d5628cbe24f0124c
anosaryev <anthonynos@outlook.com>
2022-12-12 22:40:43 -0500

    ncurses update

--- tetris.c
+++ tetris.c
@@ -2,6 +2,8 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
+#include <errno.h>
+#include <ncurses.h>
 
 #define I_PIECE 0
 #define L_PIECE 1
@@ -14,24 +16,24 @@
 int maps[7][4][4] = 
 {
     { /* I */
-        {0, 1, 0, 0},
-        {0, 1, 0, 0},
-        {0, 1, 0, 0},
-        {0, 1, 0, 0}
+        {0, 0, 0, 0},
+        {1, 1, 1, 1},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}
     }, { /* L */
-        {0, 1, 0, 0},
-        {0, 1, 0, 0},
-        {0, 1, 1, 0},
+        {0, 0, 0, 0},
+        {0, 0, 1, 0},
+        {1, 1, 1, 0},
         {0, 0, 0, 0} 
     }, { /* J */
-        {0, 0, 1, 0},
-        {0, 0, 1, 0},
-        {0, 1, 1, 0},
+        {0, 0, 0, 0},
+        {0, 1, 0, 0},
+        {0, 1, 1, 1},
         {0, 0, 0, 0}
     }, { /* T */
         {0, 0, 0, 0},
-        {1, 1, 1, 0},
         {0, 1, 0, 0},
+        {1, 1, 1, 0},
         {0, 0, 0, 0}
     }, { /* O */
         {0, 0, 0, 0},
@@ -67,6 +69,7 @@ int next_type;
 tetromino player;
 board buildup;
 
+// unused
 int print_tetromino(tetromino p1){
     for (int i = 0; i < 4; i++){
         for (int j = 0; j < 4; j++){
@@ -79,56 +82,59 @@ int print_tetromino(tetromino p1){
 // colour guide:
 // https://dev.to/tenry/terminal-colors-in-c-c-3dgc
 void print_tile(tetromino curr){
+    printw("[]");
+    /*
     switch (curr.type){
         case (I_PIECE):
-            printf("\033[36;46m  \033[0m");
+            init_pair(1, COLOR_)
+            printw("\033[36;46m  \033[0m");
             break;
         case (L_PIECE):
-            printf("\033[33;43m  \033[0m");
+            printw("\033[33;43m  \033[0m");
             break;
         case (J_PIECE):
-            printf("\033[34;44m  \033[0m");
+            printw("\033[34;44m  \033[0m");
             break;
         case (T_PIECE):
-            printf("\033[35;45m  \033[0m");
+            printw("\033[35;45m  \033[0m");
             break;
         case (O_PIECE):
-            printf("\033[37;47m  \033[0m");
+            printw("\033[37;47m  \033[0m");
             break;
         case (S_PIECE):
-            printf("\033[32;42m  \033[0m");
+            printw("\033[32;42m  \033[0m");
             break;
         case (Z_PIECE):
-            printf("\033[31;41m  \033[0m");
+            printw("\033[31;41m  \033[0m");
             break;
         default:
-            printf("bagel falafel eggs and mac and cheese");
+            printw("bagel falafel eggs and mac and cheese");
             break;
     }
+    */
 }
 
 int print_game(){
-    system("clear");
-    printf("         Command Line Tetris\n\n");
-    printf("    Top:    %d\n", 0); // and next tetromino component
-    printf("    Score:  %d\n", 0); // and next tetromino component
-    printf("    Level:  %d\n", 0); // and next tetromino component
-    printf("    Lines:  %d\n", 0); // and next tetromino component
-    printf("\n");
+    printw("         Command Line Tetris\n\n");
+    printw("    Top:    %d\n", 0); // and next tetromino component
+    printw("    Score:  %d\n", 0); // and next tetromino component
+    printw("    Level:  %d\n", 0); // and next tetromino component
+    printw("    Lines:  %d\n", 0); // and next tetromino component
+    printw("\n");
     
     int x, y, i;
     for (y = 19; y >= 0; y--){
-        printf("      %02d   ", y);
+        printw("      %02d   ", y);
         
         for (x = 0; x < 10; x++){
-            // printf("%d", x);
             // search player piece for something at coords
-            if (x - player.x < 4 && player.y - y < 4){
-                if (player.map[x - player.x][player.y - y] == 1){
+            if (x - player.x < 4 && x - player.x >= 0 &&
+                player.y - y < 4 && player.y - y >= 0){
+                if (player.map[player.y - y][x - player.x] == 1){
                     // draw player piece with colour based on type
                     print_tile(player);
                 }else{
-                    printf(". ");
+                    printw(". ");
                 }
             }else{
 
@@ -146,34 +152,38 @@ int print_game(){
                 
                 // no player or board piece
                 if (i == buildup.len){
-                    printf(". ");
+                    printw(". ");
                 }
             }
         }
-        printf("\n");
+        printw("\n");
     }
-    printf("\n           0 1 2 3 4 5 6 7 8 9\n");
+    printw("\n           0 1 2 3 4 5 6 7 8 9\n");
 }
 
 int main(){
-    system("clear");
+
+    initscr();
+    cbreak();
+    noecho();
+    clear();
+
+    int row, col;
+    getmaxyx(stdscr, row, col);
+
     srand(time(0));
 
     player.type = rand()%7;
     player.x = 3;
-    player.y = 19;
+    player.y = 20;
     memcpy(player.map, maps[player.type], sizeof(int[4][4]));
+    
+    next_type = rand()%7;
 
-    // print_tetromino(p1);
-    int i = 0;
-    while (++i){
-        print_game();
-        printf("%d\n", i);
-
-        struct timespec t0 = {0, 100000000};
-        nanosleep(&t0, NULL);
-    }
-
+    print_game();
+    
+    getch();
+	endwin();
     return 1;
 }
 
